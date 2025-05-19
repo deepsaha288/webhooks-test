e0d6eaf4bc696da1a68620b2c87bf65057ff6727
unknown <deepsaha288@yahoo.com>
2025-05-19 17:55:27 +0530

    latest code changes

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "s3_bucket_name" "test-bucket" {
     bucket = var.s3_bucket_name
     region = var.s3_bucket_region
     tags = {
-        Name        = "TestBucket"
+        Name        = "TestBucket1"
         Environment = "dev"
     }
 
